9c6a7e01bfbeb18b12c14e35eaefae17fc724dd6
Claire Campbell <cece@Claires-MBP.NetComm.Wireless>
2018-06-24 11:57:59 +1200

    adding layout component and prettier files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello world</h1>
+        <Layout><p>Hello</p></Layout>
       </div>
     );
   }
